024c489356c04c9471395710809b3fb2e37b1e41
sama_j <jordan.sama@epitech.eu>
2017-01-26 19:05:36 +0100

    list users et channel fonctionnel

--- app/js/socket.js
+++ app/js/socket.js
@@ -23,25 +23,67 @@ var socket = io();
     socket.on('chat message', function(msg){
         $('#you').append($('<p>').text(msg));
     });
+
 //Partie task
 	
 	$("#request").click(function() {
 		var value = $("#task").val();
 		if (value.indexOf("/nick") >= 0) {
-			rename(value);
+			
+			if (value != "/nick") {
+				rename(value);
+			} else {
+				socket.emit('chat message', $('#task').val());
+				$('#task').val('');
+				return false;
+			}
+
 		} else if (value.indexOf("/list") >= 0) {
-			list(value);
+
+				list(value);
+
 		} else if (value.indexOf("/join") >= 0) {
-			joinChan(value);
-		} else if (value === "/part") {
-			part();
+			
+			if (value != "/join" ) {
+				joinChan(value);
+			} else {
+				socket.emit('chat message', $('#task').val());
+				$('#task').val('');
+				return false;
+			}
+
+		} else if (value.indexOf("/part") >= 0) {
+			
+			if (value != "/part") {
+				part(value);
+			} else {
+				socket.emit('chat message', $('#task').val());
+				$('#task').val('');
+				return false;
+			}
+
 		} else if (value === "/users") {
+
 			users();
+
+		} else if (value.indexOf("/msg") >= 0) {
+			
+			if (value != "/msg") {
+				msgUser(value);
+			} else {
+				socket.emit('chat message', $('#task').val());
+				$('#task').val('');
+				return false;
+			}
+
 		} else {
-			socket.emit('chat message', $('#m').val());
+			socket.emit('chat message', $('#task').val());
+			$('#task').val('');
 			return false;
 		}
 	});
+
+	//end partie task
 
 	//keypress
 
@@ -52,14 +94,87 @@ var socket = io();
 		}
 	});
 
+	//!--keypress
+
+	//function
+
 	function rename(value) {
 		socket.emit('edit name', value);
 		$("#task").val('');
 	}
 
 	function users() {
-		var users = socket.emit('list users');
-		//console.log(users.id);
-		console.log(users); 
+		socket.emit('list users');
+
+		socket.on('listU', function(data){
+			console.log("create tab: ", data );
+
+			$("#bold").append($('<p>').text("Liste des utilisateurs:"));
+
+			for (var i = 0; i < data.length; i++) {
+				console.log("nom: ", data[i]);
+				$('#result').append($('<p>').text(data[i].name));
+			};	
+			$('#task').val('');
+		})
+
   	}
+
+  	function list(value) {
+  		socket.emit('list channel', value);
+
+  		socket.on('result channels', function(data) {
+  			if (data.length == 0) {
+  				$("#bold").val('');
+  				$("#bold").append($('<p>').text("Aucun channel disponible correspondant a votre recherche. :("));
+  			} else {
+  				$("#bold").val('');
+  				$("#result").val('');
+  				$("#bold").append($('<p>').text("Voici la liste des channels disponibles:"));
+  				for (var i = 0; i < data.length; i++) {
+  					$("#result").append($('<p>').text(data[i]));
+  				};
+
+  			}
+  		})
+  	}
+
+  	function joinChan(value) {
+  		socket.emit('join channel', value);
+
+  		socket.on('update channel', function(data){
+  			socket.emit('update', data);
+  		})
+
+  		socket.on('result join', function(data, name) {
+  			$("#bold").val('');
+  			if (data == "cool") {
+  				$("#bold").append($('<p>').text("Vous avez bien rejoint le " + name));
+  			} else {
+  				$("#bold").append($('<p>').text("Aucun channel disponible correspondant a votre recherche. :("));
+  			}
+  		})
+  	}
+
+  	function part(value) {
+  		socket.emit('leave channel', value);
+  		$("#bold").val('');
+  		socket.on('result leave', function(data, name) {
+  			if (data == "sucess"){
+  				$("#you").append($('<p>').text( name + " a quitter ce channel"));
+  			} else {
+  				$("#bold").append($('<p>').text("Aucun channel disponible correspondant a votre recherche. :("));
+  			}
+  		})
+  	}
+	
+  	function msgUser(value) {
+  		socket.emit('msg user', value);
+
+  		socket.on('send user', function(data){
+  			$("#you").append($('<p>').text(data));
+  		}) 	
+  	}
+
+  	//end function
 })
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ express.use(app.static('app'));
 
 //Username
 var users = [];
+var message = [];
 
 //Channels
 
@@ -22,17 +23,20 @@ express.get('/channel', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('adduser', function (value){
-
+  	//console.log("avant: ", users);
   	socket.pseudo = value;
-  	console.log(socket.pseudo);
+  	//console.log(socket.pseudo);
 
   	socket.channel = channels[0]; 
   	user = {
   		id: socket.id,
   		name: value,
-  		channel: channels[0],
+  		channel: [channels[0]],
+  		current: channels[0],
   	};
   	users.push(user);
+
+  	//console.log("après :" , users);
 
   	socket.join('chan1');
 
@@ -50,30 +54,145 @@ io.on('connection', function(socket){
   	//console.log(start);
   	var cut = str2.substr(start);
   	socket.pseudo = cut;
+  	
+  	//Update reference object
+  	for (var i = 0; i < users.length; i++) {
+  		if (socket.id == users[i].id) {
+  			users[i].name = socket.pseudo;
+  		};
+  	};
 
   	console.log('change: ', socket.pseudo);
 
   });
 
+  //List user in current channel
   socket.on('list users', function (){
-  	//var userList = io.sockets.clients(socket.channel);
-  	//var currentChan = io.sockets.adapter.rooms[socket.channel];
   	var res = [];
-  	io.socket.socket.map(function(user){
-  		res.push(user);
-	});
-  	return res;
-  	/*for (var i = 0; i < users.length; i++) {
-  		if (users[i.channel] == socket.channel) {
-  			res.push(users[i]);
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].current == socket.channel) {
+			res.push(users[i]);
+			console.log(users[i]);
+		};
+	};
+  	socket.emit('listU', res);
+	
+  });
+
+
+  
+
+  //List available channels
+  socket.on('list channel', function(data) {
+	var res = [];
+  	if (data == "/list") {
+  		for (var i = 0; i < channels.length; i++) {
+  			res.push(channels[i]);
+  		};
+  	} else {
+  		var str2 = data.replace(/\s+/g, '');
+  		var str = "/list";
+
+		var start = str.length;
+		var cut = str2.substr(start);
+		for (var i = 0; i < channels.length; i++) {
+			if (channels[i].indexOf(cut) >= 0) {
+				res.push(channels[i]);
+			};
+		};
+  	}
+  	
+  	socket.emit('result channels', res);
+  })
+
+  //Join channel
+  socket.on('join channel', function(data) {
+  	var statut;
+  	var str2 = data.replace(/\s+/g, '');
+  	var str = "/join";
+
+  	var start = str.length;
+  	var cut = str2.substr(start);
+  	//join
+  	for (var i = 0; i < channels.length; i++) {
+  		if(cut === channels[i]) {
+  			
+  			socket.emit('update channel', cut);
+  			
+  			return false;
+  		} else {
+  			statut = "pas cool";
+  		}
+  		
+  	};
+  	  	
+  })
+
+  //update channel reference
+  socket.on('update', function(data){
+  	var statut;
+  	for (var i = 0; i < users.length; i++) {
+  	//update reference object
+  		if (socket.id == users[i].id && users[i].channel[i] != data) {
+  			users[i].channel.push(data);
+  			users[i].current = data;
+  			socket.join(data);
+  			socket.channel = data;
+  			statut = "cool";
+  			socket.emit('result join', statut, socket.channel);
+ 		} else {
+ 			console.log("nope");
+ 			statut = "pas cool";
+ 		}
+  	};
+  	console.log('tab: ', users);
+  })
+
+  //Leave channel
+  socket.on('leave channel', function(data){
+  	var str2 = data.replace(/\s+/g, '');
+  	var str = "/part";
+
+  	var start = str.length;
+  	var cut = str2.substr(start);
+  	var statut;
+  	socket.leave(socket.channel);
+  	statut = "success";
+  	socket.emit('result leave', statut);
+
+  })
+
+  //send messages to specific user
+  socket.on('msg user', function(data) {
+  	//var str2 = data.replace(/\s+/g, '');
+  	//var "/msg ";
+
+  	if (data.substr(0, 5) === "/msg ") {
+  		data = data.substr(5);
+  		var val = data.indexOf(" ");
+  		
+  		if (val != -1) {
+  			var nom = data.substring(0, val);
+			var msg = data.substring(val + 1);  		
+  		};	
+  	};
+
+  	
+  	
+  	console.log("nom: ", nom);
+  	console.log("msg: ", msg);
+  	for (var i = 0; i < users.length; i++) {
+  		if (socket.channel == users[i].channel[i] && nom == users[i].name ) {
+  			var mesu = users[i].name;
+  			mesu.io.socket.emit('send user', socket.pseudo + " to " + name + ": " + msg);
   		};
   	};
-  	return res;*/
-	
-  });   
 
+  })
+
+  //Send message
   socket.on('chat message', function(msg){
-    io.emit('chat message', socket.username + " dit: " + msg);
+    io.emit('chat message', socket.pseudo + " dit: " + msg);
   });
 
   socket.on('disconnect', function(){
